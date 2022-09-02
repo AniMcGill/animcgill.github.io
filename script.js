f2952e3c4b891daa90247bfcb9273b3f67f3e3bf
Kevin Yu <44329173+iveykun@users.noreply.github.com>
2022-09-02 11:16:21 -0400

    changed github link to the new link

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ class MyFooter extends HTMLElement {
       -->
 
 <div class= "third-box">
-   <p class="made-with-love"> Made with 🤎 by Christina, advised by Abu, and art by Song. <a href="https://github.com/bluejam100/MSAC_website
+   <p class="made-with-love"> Made with 🤎 by Christina, advised by Abu, and art by Song. <a href="https://github.com/AniMcGill/animcgill.github.io
 "> GitHub Repo</a>  </p>
 </div>
     
