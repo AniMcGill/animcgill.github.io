ea953a969278f50fd63c73409758b4b75fe47d70
39bytes <zhangjeff2014@gmail.com>
2024-05-07 19:06:55 -0400

    switch to dayjs to reduce bundle size

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,33 +1,33 @@
-import { format } from 'date-fns-tz';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const EST = 'America/Montreal';
 const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const formatEventDate = (start: number, end: number, short = false) => {
-  const startDate = new Date(start * 1000);
-  const endDate = new Date(end * 1000);
+  const startTime = dayjs.unix(start).tz(EST);
+  const endTime = dayjs.unix(end).tz(EST);
+  const diff = end - start;
 
-  const diff = endDate.getTime() - startDate.getTime();
   if (diff > ONE_DAY) {
-    const formatString = short ? 'eee PP' : 'eee PPp';
-    const startFormatted = format(startDate, formatString, {
-      timeZone: 'America/Montreal',
-    });
+    const formatString = short ? 'ddd MMM D h:mm A' : 'ddd MMM D h:mm';
+    const startFormatted = startTime.format(formatString);
 
     if (short) {
       return startFormatted;
     }
 
-    const endFormatted = format(endDate, formatString, {
-      timeZone: 'America/Montreal',
-    });
+    const endFormatted = endTime.format(formatString);
 
     return `${startFormatted} - ${endFormatted}`;
   }
 
-  const startFormatted = format(startDate, 'eee PPp', {
-    timeZone: 'America/Montreal',
-  });
-  const endFormatted = format(endDate, 'p', { timeZone: 'America/Montreal' });
+  const startFormatted = startTime.format('ddd MMM D h:mm A');
+  const endFormatted = endTime.format('h:mm A');
 
   return `${startFormatted} - ${endFormatted}`;
 };
